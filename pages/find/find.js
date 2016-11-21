da3a55b9dc9959582ffcccaf863aa316827aaea7
zhen.lei <zhen.lei@kindroid.com>
2016-11-21 23:02:35 +0800

    发现模块

--- pages/find/find.js
+++ pages/find/find.js
@@ -1,73 +1,70 @@
 Page({
-  data:{
-    list:[
-            {
-              list_tool:[
-                          {
-                              img:"/image/friend_r.png",
-                              name:"朋友圈",
-                              url:""
-                            }
-                          ]
-            },
-            {
-              list_tool:[
-                          {
-                            img:"/image/saoyisao.png",
-                            name:"扫一扫",
-                            url:""
-                    },
-                    { 
-                            img:"/image/yaoyiyao.png",
-                            name:"摇一摇",
-                            url:""
-                    }
-                ] 
-            },
-            {
-              list_tool:[
-                          {
-                            img:"/image/newFriend.png",
-                            name:"附近的人",
-                            url:""  
-                          },
-                          {
-                            img:"/image/piaoliuping.png",
-                            name:"漂流瓶",
-                            url:""
-                          }
+    data: {
+        list: [{
+            list_tool: [{
+                img: "/image/friend_r.png",
+                name: "朋友圈",
+                url: ""
+            }]
+        }, {
+            list_tool: [{
+                img: "/image/saoyisao.png",
+                name: "扫一扫",
+                url: ""
+            }, {
+                img: "/image/yaoyiyao.png",
+                name: "摇一摇",
+                url: ""
+            }]
+        }, {
+            list_tool: [{
+                    img: "/image/newFriend.png",
+                    name: "附近的人",
+                    url: ""
+                }, {
+                    img: "/image/piaoliuping.png",
+                    name: "漂流瓶",
+                    url: ""
+                }
 
-              ] 
-            },
-            {
-              list_tool:[
-                          {
-                            img:"/image/gouwu.png",
-                            name:"购物",
-                            url:""
-                          },
-                          {
-                            img:"/image/gouwu.png",
-                            name:"游戏（现在是prcker）",
-                            url:"../prcker/prcker"
-                          }
-              ]
-            }
-          ]
-  },
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+            ]
+        }, {
+            list_tool: [{
+                img: "/image/gouwu.png",
+                name: "购物",
+                url: ""
+            }, {
+                img: "/image/gouwu.png",
+                name: "游戏（现在是prcker）",
+                url: "../prcker/prcker"
+            }]
+        }]
+    },
+
+    goPage: function(event) {
+        //新窗口打开
+        var tourl = event.currentTarget.dataset.url
+        if (tourl != '') {
+            wx.navigateTo({
+                url: event.currentTarget.dataset.url
+            })
+        }
+        return false
+    },
+
+    onLoad: function(options) {
+        // 页面初始化 options为页面跳转所带来的参数
+    },
+    onReady: function() {
+        // 页面渲染完成
+    },
+    onShow: function() {
+        // 页面显示
+    },
+    onHide: function() {
+        // 页面隐藏
+    },
+    onUnload: function() {
+        // 页面关闭
+    }
 })
